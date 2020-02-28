9f7c6f9784f48863349e5ba77f227ce06fa281de
Igrey1321 <igrey1321@gmail.com>
2020-02-28 19:07:50 +0200

    add login

--- js/app.js
+++ js/app.js
@@ -1,22 +1,30 @@
 let hash = '';
 
 const [ name, email, passOne, passTwo, submit, signup, ignupButton,
-   avatar, img, loginIn, passIn, submitIn] =
+   avatar, img, loginIn, passIn, submitIn, log_in, loginButton, imgNone] =
     [ 'name', 'email', 'pass_one', 'pass_two', 'submit', 'signup', 'ignupButton',
-    'get_avatar', 'avatar', 'login_in', 'pass_in', 'submit_in' ]
+    'get_avatar', 'avatar', 'login_in', 'pass_in', 'submit_in', 'log_in', 'loginButton', 'img_none' ]
         .map ( item => document.getElementById ( item ) )
 
 passTwo.disabled = true;
 submit.disabled = true;
 
 signup.style.display = 'none'
+log_in.style.display = 'none'
+
 signupButton.onclick = () => {
   signup.style.display != 'none' ? signup.style.display = 'none' : signup.style.display = 'block'
+    , log_in.style.display = 'none'
 };
+loginButton.onclick = () => {
+  log_in.style.display != 'none' ? log_in.style.display = 'none' : log_in.style.display = 'block'
+    , signup.style.display = 'none'
+};
+
 email.onchange = () => {
   event.target.test = Boolean(event.target.value.length >= 5
     && event.target.value.match(/^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/));
-  event.target.style.color = event.target.test ? 'green' : 'red';
+  event.target.style.border = event.target.test ? '1px solid green' : '1px solid  red';
 }
 passOne.oninput = () => {
   event.target.test = Boolean(event.target.value.length >= 8
@@ -69,12 +77,12 @@ avatar.onchange = () => {
   let data;
   const reader = new FileReader();
   reader.onload = () => {
+    imgNone.style.background = 'none';
     img.src = data = event.target.result;
     img.style.opacity = ('1');
   };
   reader.readAsDataURL(event.target.files[0]);
 };
-// fetch(`https://garevna-rest-api.glitch.me/user/${name}`);
 
 // formLogin
 
